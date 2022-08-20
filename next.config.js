782a9bcca2d5c2daed56019db4350107fa2718c9
enginesa <engin1218@hotmail.com>
2022-08-20 17:47:53 +0300

    config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    i18n: {
-        locales: ["ar", "en", "tr"],
-        defaultLocale: "en",
-    },
 };
 
 module.exports = nextConfig;
